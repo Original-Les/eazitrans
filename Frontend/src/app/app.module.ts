a571eb122e8cf6007356e9a6e93d0f7dd850a132
Kubiti <kubitisam18@gmail.com>
2020-10-28 17:40:07 +0000

    Service workers

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { RegistrationComponent } from './registration/registration.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Frontend/src/sw.js
+++ Frontend/src/sw.js
@@ -1,7 +1,14 @@
 // install service worker
 self.addEventListener('install', evt => {
+    evt.waitUntil(installServiceWorker);
     console.log('service worker has been installed');
 });
+
+async function installServiceWorker() {
+    const cache = await caches.open('app-cache');
+
+    cache.put(request, response)
+}
 
 // activate event 
 self.addEventListener('activate', evt => {
